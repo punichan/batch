6d61f1ff2f2469ac1989766712ad922f0381ee2b
punichan <masamikai4580@gmail.com>
2020-04-28 09:34:08 +0900

    fix

--- batch_test3.bat
+++ batch_test3.bat
@@ -1,16 +1,30 @@
 @echo off
+REM Windowsサービス稼働の確認
+REM set LISTFILE=C:\Users\まさみ\Documents\GitHub\batch\batch_test3.lst
+REM for /f %%a in (%LISTFILE%) do (
+REM 	sc query %%a | findstr RUNNING
+REM )
 
-for /f %%a in (batch_test3.lst) do (
+set LISTFILE=%~dp0batch_test3.lst
+
+for /f %%a in (%LISTFILE%) do (
 	sc query %%a | findstr RUNNING
 )
 
-for /f %%a in (batch_test3.lst) do (
-	net stop %%a
-	if errorlevel 1 (
+REM Windowsサービス停止
+for /f %%b in (%LISTFILE%) do (
+	net stop %%b
+	set RETURN=%ERRORLEVEL%
+
+	REM Windowsサービス停止失敗、10秒停止後、再度停止
+	if %RETURN% neq 0 (
 		timeout /NOBREAK 10
-		net stop %%a
-		if errorlevel 1(
-			echo %%a停止失敗しました
+		net stop %%b
+		set RETURN=%ERRORLEVEL%
+		
+		if %RETURN% neq 0 (
+			echo %%b停止失敗しました
+			exit /b 1
 		)
 	)
 )
