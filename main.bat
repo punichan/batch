82b6e9e53228afc1e77bfab33be247bc12d83de1
punichan <masamikai4580@gmail.com>
2020-04-28 10:50:08 +0900

    main.batの参考追加

--- main.bat
+++ main.bat
@@ -1,4 +1,7 @@
-call %~dp0batch_test2.bat >  %~dp0log.txt 2>&1
+call %~dp0batch_test3.bat >  %~dp0log.txt 2>&1
 
 pause
 exit
+
+参考
+バッチファイル実行ログの取り方：https://language-and-engineering.hatenablog.jp/entry/20130502/PatternsOfMSDOSorBAT#LogsAndRedirects
