35b8b279435638df44fef435d5dce9870488889f
punichan <masamikai4580@gmail.com>
2020-04-28 10:45:03 +0900

    比較演算子の修正

--- batch_test3.bat
+++ batch_test3.bat
@@ -17,12 +17,12 @@ for /f %%b in (%LISTFILE%) do (
 	set RETURN=%ERRORLEVEL%
 
 	REM Windowsサービス停止失敗、10秒停止後、再度停止
-	if %RETURN% neq 0 (
+	if "%RETURN%" neq "0" (
 		timeout /NOBREAK 10
 		net stop %%b
 		set RETURN=%ERRORLEVEL%
 		
-		if %RETURN% neq 0 (
+		if "%RETURN%" neq "0" (
 			echo %%b停止失敗しました
 			exit /b 1
 		)
