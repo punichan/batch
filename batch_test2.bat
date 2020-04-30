26672875814baea89ef012ea5da0dd94128f30e0
punichan <masamikai4580@gmail.com>
2020-04-30 10:52:43 +0900

    時刻同期のエラー修正し、正常動作

--- batch_test2.bat
+++ batch_test2.bat
@@ -1,25 +1,25 @@
 @echo off
-REM Windowsタイムサービスの確認
+REM Windowsタイムサービス起動確認
 sc query w32time | findstr RUNNING
 set RETURN=%ERRORLEVEL%
 
 if "%RETURN%" neq "0" (
-	REM　Windowsタイムサービスが起動していない場合は起動する。
+    REM Windowsタイムサービスが起動していない場合は、起動する。
 	net start w32time
 	set RETURN=%ERRORLEVEL%
-
-		if "%RETURN%" neq "0" (
-			REM 起動失敗
-			echo Windowsタイムサービスの起動に失敗しました。
-			exit /b 1
-		)
+	
+	if "%RETURN%" neq "0" (
+		REM 起動失敗
+		echo WindowsTimeサービスの起動に失敗しました。
+		exit /b 1
+	)
 )
 
 REM 時刻同期
 w32tm /resync
 set RETURN=%ERRORLEVEL%
 
-if %RETURN% neq 1 (
+if "%RETURN%" neq "0" (
 	REM 時刻同期失敗
 	echo 時刻同期に失敗しました。
 	exit /b 1
@@ -28,12 +28,4 @@ if %RETURN% neq 1 (
 REM 時刻同期確認
 w32tm /query /status
 
-exit 0
-
-
-rem 参考
-rem 管理者権限 https://www.adminweb.jp/command/ini/index12.html
-rem 条件分岐 https://www.adminweb.jp/command/bat/index8.html#section3
-rem 時刻同期設定 https://spiral.hatenadiary.org/entry/20110310/1327558925
-rem サービスのステータス https://tipstour.net/command/8289#i-3
-rem errorlevelについて https://jj-blues.com/cms/command-errorlevel/
+exit /b 0
