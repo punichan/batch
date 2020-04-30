1895ca690aa1a18a5863bb22daca18ce500b4d1c
punichan <masamikai4580@gmail.com>
2020-04-30 10:33:40 +0900

    停止確認と終了判定の修正

--- batch_test4.bat
+++ batch_test4.bat
@@ -2,32 +2,52 @@
 setlocal enabledelayedexpansion
 
 set LISTFILE=%~dp0batch_test4.lst
-
+set FALL_FLG=0
 REM Windowsサービスの停止確認
 for /f %%a in (%LISTFILE%) do (
 	sc query %%a | findstr STOPPED
-)
-
-REM Windowsサービスの起動
-for /f %%b in (%LISTFILE%) do (
-	net start %%b
 	set RETURN=%ERRORLEVEL%
 
-		REM サービスの再起動
+	REM 対象のサービスが停止している場合は起動
+	if "%RETURN%" neq "0" (
+		net start %%a
+		set RETURN=%ERRORLEVEL%
+
+		REM　起動しない場合10秒後経過後に再起動
 		if "!RETURN!" neq "0" (
 			timeout /NOBREAK 10
-			net start %%b
+			net start %%a
 			set RETURN=%ERRORLEVEL%
 
-				REM　再起動失敗、異常終了
-				if "!RETURN!" neq "0" (
-					echo サービスの起動に失敗しました。
-					exit /b 1
-				)
-		)
+			REM 起動失敗時、FALL_FLGを1にする
+			if "!RETURN!" neq "0" (
+				echo %%a停止に失敗しました
+				set FALL_FLG=1
+			)
+		) 
+	)
 )
 
-REM batch_test2.bat呼び出し
-call  %~dp0batch_test2.bat
+REM 終了時判定
+if "!FALL_FLG!" equ "0" (
+	REM batch_test2.bat呼び出し
+	call  %~dp0batch_test2.bat
+	set RETURN=%ERRORLEVEL%
+
+		REM batch_test2終了判定
+		if "!RETURN!" equ "0" (
+			echo 正常終了
+			exit /b 0
+		) else (
+			echo 異常終了
+			exit /b 1
+		)
+
+) else (
+	echo 異常終了
+	exit /b 1
+)
+
+
 
 exit 0
