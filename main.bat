97c9e694a76348e206db9e35d32d64307be598c5
punichan <masamikai4580@gmail.com>
2020-04-30 10:38:50 +0900

    .gitignoreに反映させるため。

--- main.bat
+++
@@ -1,7 +0,0 @@
-call %~dp0batch_test3.bat >  %~dp0log.txt 2>&1
-
-pause
-exit
-
-参考
-バッチファイル実行ログの取り方：https://language-and-engineering.hatenablog.jp/entry/20130502/PatternsOfMSDOSorBAT#LogsAndRedirects
