6ae2e890170282e38f8ac087a2db957e3c729a91
punichan <masamikai4580@gmail.com>
2020-05-01 10:23:41 +0900

    リネーム実行追加

--- batch_test5.bat
+++ batch_test5.bat
@@ -1,4 +1,10 @@
 @echo off
 
+REM バックアップされた日付
+set REN_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
+
 REM WindowsServerバックアップ実行
 wbadmin start backup -backuptarget:E: -include:C:
+
+REM バックアップファイルのリネーム
+ren E:\WindowsImageBackup\Win2016 Win2016_%REN_DATE%
