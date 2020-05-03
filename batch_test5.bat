754d94e7bd3c9c37aca0f318899bd7a3803a765b
punichan <masamikai4580@gmail.com>
2020-05-03 17:49:48 +0900

    変数の位置を変更

--- batch_test5.bat
+++ batch_test5.bat
@@ -1,17 +1,18 @@
 @echo off
 
-set BACKUP_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
-set BACKUP_FLD=E:\WindowsImageBackup
-set COUNT=2
-
 REM WindowsServerバックアップ実行
 wbadmin start backup -backuptarget:E: -include:C:
 
 REM バックアップファイルのリネーム
-ren E:\WindowsImageBackup\Win2016 Win2016_%REN_DATE%
+set BACKUP_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
+set BACKUP_FLD=E:\WindowsImageBackup
+
+if 
+ren %BACKUP_FLD%\Win2016 Win2016_%REN_DATE%
 
 REM 降順にして、3世代保管
-for /d "skip=%COUNT%" %%a in ('dir %BACKUP_FLD% /b /o-n') do (
+set COUNT=2
+for /f "skip=%COUNT%" %%a in ('dir %BACKUP_FLD% /b /o-n') do (
 	rmdir /s /q %BACKUP_FLD%\%%a
 )
 
