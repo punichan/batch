4e675ee460e048ae5624ee2d59ac023d40a5d233
punichan <masamikai4580@gmail.com>
2020-05-03 17:55:53 +0900

    ファイル名の確認追加 ファイル名があった場合はバックアップ不可

--- batch_test5.bat
+++ batch_test5.bat
@@ -1,17 +1,28 @@
 @echo off
 
+set BACKUP_FLD=E:\WindowsImageBackup
+
+REM WIN2016というファイル名があるかを確認
+dir /b %BACKUP_FLD% | findstr "Win2016"
+set RETURN=%ERRORLEVEL%
+
+REM WIN2016というファイル名があればバックアップ不可
+if %RETURN% equ 0 (
+	echo Win2016というフォルダが存在します。バックアップができません。
+	exit /b 1 
+)
+
 REM WindowsServerバックアップ実行
 wbadmin start backup -backuptarget:E: -include:C:
 
 REM バックアップファイルのリネーム
 set BACKUP_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
-set BACKUP_FLD=E:\WindowsImageBackup
 
-if 
 ren %BACKUP_FLD%\Win2016 Win2016_%REN_DATE%
 
 REM 降順にして、3世代保管
 set COUNT=2
+
 for /f "skip=%COUNT%" %%a in ('dir %BACKUP_FLD% /b /o-n') do (
 	rmdir /s /q %BACKUP_FLD%\%%a
 )
