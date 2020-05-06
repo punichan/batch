470b5b345a7bb029065c8bf28eccf4a103f293e7
punichan <masamikai4580@gmail.com>
2020-05-06 16:43:09 +0900

    参考追加

--- batch_test8.bat
+++ batch_test8.bat
@@ -3,3 +3,5 @@ echo zipファイル作成します。
 powershell -NoProfile -ExecutionPolicy Unrestricted .\batch_test8.ps1
 echo zipファイルが作成されました。
 exit /b 0
+
+REM powershellの権限変更参考：https://qiita.com/tomoko523/items/df8e384d32a377381ef9
