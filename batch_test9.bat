0451d82e2edf65290c0a42d8caa5a265de113e8f
punichan <masamikai4580@gmail.com>
2020-05-06 19:00:34 +0900

    ドライブの存在確認追加

--- batch_test9.bat
+++ batch_test9.bat
@@ -11,8 +11,17 @@ if "%1" equ "" (
 )
 
 REM 引数を変数にセットする
-set DRVNAME=%1
-echo %1
+set DRVNAME=%1:
 
 REM 実行日時をlogに出力する
 echo %date% > %~dp0batch_test9.log
+
+REM 引数で与えられたドライブの存在確認・なければ異常終了
+if not exist "%DRVNSME%" (
+    echo 指定されたドライバは存在しません。
+    echo 指定されたドライバは存在しません。 > %~dp0batch_test9.log
+    exit /b 1
+) else (
+    
+)
+
