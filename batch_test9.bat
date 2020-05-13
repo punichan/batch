e2a6a98ed9baa01681ad00b8cb07ddbd89586107
punichan <masamikai4580@gmail.com>
2020-05-13 12:12:08 +0900

    %WARN%との比較を追加

--- batch_test9.bat
+++ batch_test9.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
+setlocal enabledelayedexpansion
 
 REM 環境変数をセットする
 set WARN=80
@@ -44,6 +44,40 @@ REM 100からNUM1を引き算する
 set /a NUM3=100-!NUM1!
 echo %NUM3%
 
+REM NUM3と、変数CRITの値を比較する。
+if %NUM3% geq %CRIT% (
+    REM NUM3のほうが%CRIT%以上の場合
+    echo ディスク使用率がクリティカルの値を超えています。
+    echo ディスク使用率がクリティカルの値を超えています。 > %DRVNAME%\Work\batch_test9.log
+    del /q %DRVNAME%\Work\batch_test9.txt.log
+    del /q %DRVNAME%\Work\batch_test9.txt
+    echo 1
+
+) else if %NUM3% lss %CRIT% (
+    REM NUM3のほうが%CRIT%未満の場合
+    echo 2
+
+    if %NUM3% geq %WARN% (
+        REM NUM3のほうが%WARN%以上の場合
+        echo ディスク使用率が警告の値を超えています。
+        echo ディスク使用率が警告の値を超えています。 > %DRVNAME%\Work\batch_test9.log
+        del /q %DRVNAME%\Work\batch_test9.txt.log
+        del /q %DRVNAME%\Work\batch_test9.txt
+        echo 3
+
+    ) else if %NUM3% lss %WARN% (
+        REM NUM3のほうが%WARN%未満の場合
+        echo ディスク使用率は閾値超えていません。
+        echo ディスク使用率は閾値超えていません。 > !DRVNAME!\Work\batch_test9.log
+        del /q %DRVNAME%\Work\batch_test9.txt.log
+        del /q %DRVNAME%\Work\batch_test9.txt
+        echo 4
+    )
+)
+
+echo 5
+exit /b 0
+
 REM カウンター（FREE SPACE）参考：https://www.sskpc.net/sskpc/w2003s/ch6/6_7_1.html
 REM findstrコマンドのオプション参考：https://www.k-tanaka.net/cmd/findstr.php
 REM typeperfコマンド参考：https://4thsight.xyz/14738
@@ -51,3 +85,5 @@ REM 変数から""を取り除く参考：https://qiita.com/tomotagwork/items/5b
 REM echoはoffです解消参考：https://www.google.com/search?sxsrf=ALeKk01xlJL5VFeEdqWDIAYvgy8oWcP6NA%3A1588990955034&ei=6xO2XqjXAaTfmAXI6IKICQ&q=echo%E3%81%AFoff%E3%81%A7%E3%81%99&oq=echo%E3%81%AF&gs_lcp=CgZwc3ktYWIQARgAMgIIADICCAAyBAgAEAQyBAgAEAQyAggAMgIIADoHCAAQRhD_AToHCCMQ6gIQJzoECCMQJzoHCAAQgwEQBDoECAAQQzoFCAAQgwFQqftHWKWhSGDBs0hoAnAAeAGAAfoDiAGCF5IBCTAuNi42LjUtMZgBAKABAaoBB2d3cy13aXqwAQo&sclient=psy-ab
 REM for /fの使い方：https://www.lisz-works.com/entry/bat-split-for-f
 REM バッチ内での計算：https://jj-blues.com/cms/wantto-calculationinbatfile/
+REM 比較演算子：https://qiita.com/plcherrim/items/8edf3d3d33a0ae86cb5c
+REM ファイル削除の仕方：https://www.k-tanaka.net/cmd/del.php
