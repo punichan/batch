cad0eda2eb0772b854daf3b65ec4dccdcc7f601a
punichan <masamikai4580@gmail.com>
2020-06-05 12:29:58 +0900

    修正

--- batch_test12.bat
+++ batch_test12.bat
@@ -16,33 +16,14 @@ REM ############################################################################
 
 REM イベントログフォルダ
 set LOG_FO = C:\log\eventlog
-REM ファイル作成日
-set yy=%date:~0,4%
-set mm=%date:~5,2%
-set dd=%date:~8,2%
-set /a dd=%dd%-1
-set dd=00%dd%
-set dd=%dd:~-2%
-if %dd%==00 (
-if %mm%==01 (set mm=12&& set dd=31&& set /a yy=%yy%-1)
-if %mm%==02 (set mm=01&& set dd=31)
-if %mm%==03 (set mm=02&& set dd=28&& if %ymod%==0 (set dd=29))
-if %mm%==04 (set mm=03&& set dd=31)
-if %mm%==05 (set mm=04&& set dd=30)
-if %mm%==06 (set mm=05&& set dd=31)
-if %mm%==07 (set mm=06&& set dd=30)
-if %mm%==08 (set mm=07&& set dd=31)
-if %mm%==09 (set mm=08&& set dd=31)
-if %mm%==10 (set mm=09&& set dd=30)
-if %mm%==11 (set mm=10&& set dd=31)
-if %mm%==12 (set mm=11&& set dd=30)
-)
+
 
 REM C:\log\eventlogがない場合
 if not exist %LOG_FO% (
     REM C:\log\eventlogを作成する。
     md log/eventlog
 )
+
 
 REM ####################################################################################################
 REM #参考サイト一覧
@@ -63,16 +44,20 @@ REM 2.イベントの存在確認
 REM 昨日のAPPイベントの検索
 REM APPイベントが存在する場合
 REM ファイルの作成
+REM フラグを立てる
 
 REM 昨日のSYSイベントの検索
 REM SYSイベントが存在する場合
 REM ファイルの作成
+REM フラグを立てる
 
 REM 昨日のSECイベントの検索
 REM SECイベントが存在する場合
 REM ファイルの作成
+REM フラグを立てる
 
-REM 3.戻り値の判定
+REM 3.フラグの判定
+
 
 
 REM ####################################################################################################
